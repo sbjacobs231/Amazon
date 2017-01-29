a43283fb0ad578fc113fee4eeddc40b5ad82be2e
Sky Jacobson <sky.ishitani231@gmail.com>
2017-01-29 00:01:02 -0800

    Search Bar can searches

--- js/main.js
+++ js/main.js
@@ -21,3 +21,29 @@ $(mouseOn).mouseleave(function() {
 $('.goUp').click(function(){
 	$(document).scrollTop(0)
 });
+
+// Search button
+function searchTerm() {
+	var link = "https://www.amazon.com/s?field-keywords=";
+	var searchVal = $('.searchBar').val();
+	var searchAlter = "";
+	for (var i = 0; i < searchVal.length; i++) {
+		if (searchVal[i] == " ") {
+			searchAlter += "+";
+		} else {
+			searchAlter += searchVal[i];
+		}
+	}
+	link = link + searchAlter;
+	$('.holmes').attr('href', link);
+	console.log("keyed up")
+};
+
+$('.holmes').on('click', searchTerm);
+
+// Keypress search button
+$(".searchBar").on("keyup", function(event) {
+	if (event.keyCode == 13) {
+		$('.holmes')[0].click();
+	}
+})
